d75445210747640e1a9f14d37979d0de07ee32d8
LydSleepyGhost <152915700+LydSleepyGhost@users.noreply.github.com>
2024-02-15 23:50:53 -0500

    Add files via upload

--- PizzaApp.java
+++ PizzaApp.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-public class PizzaApp extends JFrame
+public class PizzaApp extends JFrame implements ItemListener
 {
    JLabel size = new JLabel("Select your Pizza Size");
    JButton small = new JButton("Small [$5.00]");
@@ -13,7 +13,7 @@ public class PizzaApp extends JFrame
    JButton large = new JButton("Large [$15.00]");
    JButton supersize = new JButton("Super [$20.00]");
    Font bigfont = new Font("Ariel", Font.BOLD, 16);
-   JLabel total = new JLabel("Your total currently is:");
+   JLabel total = new JLabel("Your total currently is: " + moneytotal);
    JLabel toppings = new JLabel("What toppings would you like? (Pick 3)");
    JCheckBox pepperoni = new JCheckBox("Pepperoni", false);
    JCheckBox olives = new JCheckBox("Olives", false);
@@ -26,11 +26,11 @@ public class PizzaApp extends JFrame
    JCheckBox extra_cheese = new JCheckBox("Extra Cheese", false);
    JButton next = new JButton("Next");
 
-   float moneytotal = 0;
+   static float moneytotal = 0;
    int counter = 0;
 
-   final int Width = 300;
-   final int Hight = 200;
+   final int Width = 500;
+   final int Hight = 300;
 
    public PizzaApp()
    {
@@ -43,14 +43,16 @@ public class PizzaApp extends JFrame
       add(medium);
       add(large);
       add(supersize);
-      add(next);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);
       small.addActionListener(new ActionListener()
       {
          @Override
          public void actionPerformed(ActionEvent e)
          {
             moneytotal += 5;
+            PizzaApptwo();
+
          }
       });
       medium.addActionListener(new ActionListener()
@@ -59,6 +61,7 @@ public class PizzaApp extends JFrame
          public void actionPerformed(ActionEvent e)
          {
             moneytotal += 10;
+            PizzaApptwo();
 
          }
       });
@@ -68,6 +71,7 @@ public class PizzaApp extends JFrame
          public void actionPerformed(ActionEvent e)
          {
             moneytotal += 15;
+            PizzaApptwo();
          }
       });
       supersize.addActionListener(new ActionListener()
@@ -76,22 +80,13 @@ public class PizzaApp extends JFrame
          public void actionPerformed(ActionEvent e)
          {
             moneytotal += 20;
+            PizzaApptwo();
          }
       });
-      next.addActionListener(new ActionListener()
-      {
-         @Override
-         public void actionPerformed(ActionEvent e)
-         {
-            new PizzaApptwo();
-         }
-      });
-      //setVisable(true);
    }
 
-   public PizzaApptwo()
+   public void PizzaApptwo()
    {
-      super("Pizza App cont...");
       setSize(Width, Hight);
       setLayout(new FlowLayout());
       toppings.setFont(bigfont);
@@ -104,8 +99,11 @@ public class PizzaApp extends JFrame
       add(sausage);
       add(mushrooms);
       add(extra_cheese);
-      add(next);
-      pepperoni.addItemListener((ItemListener) this);
+      add(total);
+      next.setVisible(false);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);
+      pepperoni.addItemListener( this);
       olives.addItemListener((ItemListener) this);
       peppers.addItemListener((ItemListener) this);
       pineapple.addItemListener((ItemListener) this);
@@ -116,11 +114,12 @@ public class PizzaApp extends JFrame
       extra_cheese.addItemListener((ItemListener) this);
       next.addItemListener((ItemListener) this);
 
-      pepperoni.addItemListener(new ItemListener()
+     /* pepperoni.addItemListener(new ItemListener()
       {
          @Override
          public void itemStateChanged(ItemEvent e)
          {
+            moneytotal += .5;
 
          }
 
@@ -130,6 +129,7 @@ public class PizzaApp extends JFrame
          @Override
          public void itemStateChanged(ItemEvent e)
          {
+            moneytotal += .5;
          }
 
       });
@@ -138,6 +138,7 @@ public class PizzaApp extends JFrame
          @Override
          public void itemStateChanged(ItemEvent e)
          {
+            moneytotal += .5;
          }
 
       });
@@ -146,6 +147,7 @@ public class PizzaApp extends JFrame
          @Override
          public void itemStateChanged(ItemEvent e)
          {
+            moneytotal += .5;
          }
 
       });
@@ -154,6 +156,7 @@ public class PizzaApp extends JFrame
          @Override
          public void itemStateChanged(ItemEvent e)
          {
+            moneytotal += .5;
          }
 
       });
@@ -162,6 +165,7 @@ public class PizzaApp extends JFrame
          @Override
          public void itemStateChanged(ItemEvent e)
          {
+            moneytotal += .5;
          }
 
       });
@@ -170,6 +174,7 @@ public class PizzaApp extends JFrame
          @Override
          public void itemStateChanged(ItemEvent e)
          {
+            moneytotal += .5;
          }
 
       });
@@ -178,6 +183,7 @@ public class PizzaApp extends JFrame
          @Override
          public void itemStateChanged(ItemEvent e)
          {
+            moneytotal += .5;
          }
 
       });
@@ -188,23 +194,42 @@ public class PizzaApp extends JFrame
          {
          }
 
-      });
+      });*/
       next.addItemListener(new ItemListener()
       {
          @Override
          public void itemStateChanged(ItemEvent e)
          {
+            System.out.println("Total is Final " + moneytotal);
          }
 
       });
-      //setVisable(true);
+
    }
+   @Override
+   public void itemStateChanged(ItemEvent e) {
+      Object source = e.getItem();
+      String sTopping = ((JCheckBox)source).getText();
+      System.out.println("Checkbox " + e.getStateChange());
+      if(sTopping != "Extra Cheese") {
+         System.out.println("Topping Selected " + sTopping);
+         int select = e.getStateChange();
+         if (select == ItemEvent.SELECTED) {
+            moneytotal += .5;
+         }
+         else
+            moneytotal -= .5;
+      }
+      else
+         System.out.println("You added " + sTopping + " so no charge");
+      total.setText("Total is After " + moneytotal);
+      //System.out.println("Total is After " + moneytotal);
+   }
+
 
    public static void main(String[] args)
    {
-      new PizzaApp();
-      //setVisable(true);
-
+      PizzaApp myPizza = new PizzaApp();
    }
 
 }
